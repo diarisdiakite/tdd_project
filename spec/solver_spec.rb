3eb0b29eac514e12fec415dab887516ab1e83f41
diarisdiakite <diarisdiakite@gmail.com>
2023-10-13 09:05:25 +0200

    Add test for the reverse method.

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -17,4 +17,18 @@ describe Solver do
       expect { solver.factorial(-3) }.to raise_error(ArgumentError)
     end
   end
+
+  describe "#reverse" do
+    it "returns a character when the input length is 1" do
+      expect(solver.reverse('h')).to eq('h')
+    end
+  
+    it "returns the reverse of a given string" do
+      expect(solver.reverse('Hello')).to eq('olleH')
+    end
+  
+    it "raises an exception when the string length is 0" do
+      expect { solver.reverse('') }.to raise_error(ArgumentError)
+    end
+  end
 end
